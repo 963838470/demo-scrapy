8c4c7b059d84d40f1cc594949c2468b64706cafd
r720 <963838470@qq.com>
2017-12-30 18:47:15 +0800

    获取腾讯所有职位实体

--- Tencent/Tencent/items.py
+++ Tencent/Tencent/items.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
+'''爬取实体类'''
 import scrapy
 
 
 class TencentItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    '''定义实体类'''
+    positionName = scrapy.Field()  # 职位名称
+    positionLink = scrapy.Field()  # 职位详情链接
+    positionType = scrapy.Field()  # 职位类别
+    recruitNumber = scrapy.Field()  # 招聘人数
+    workLocation = scrapy.Field()  # 工作地点
+    publishTime = scrapy.Field()  # 发布时间
---
+++ Tencent/Tencent/spiders/tencent.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+'''爬虫执行页面'''
+import scrapy
+from Tencent.items import TencentItem
+
+
+class TencentSpider(scrapy.Spider):
+    ''' 将页面的数据写入实体 '''
+    name = 'tencent'
+    allowed_domains = ['tencent.com']
+    baseURL = 'http://hr.tencent.com/position.php?&start'
+    offset = 0
+    start_urls = [baseURL + str(offset)]
+
+    def parse(self, response):
+        nodes = response.xpath("//tr[@class='even'] | //tr[@class='odd']")
+        for node in nodes:
+            item = TencentItem()
+            item['positionName'] = node.xpath("./td[1]/a/text()").extract()[0]
+            item['positionLink'] = node.xpath("./td[1]/a/@href").extract()[0]
+            item['positionType'] = node.xpath("./td[2]/text()").extract()[0]
+            item['recruitNumber'] = node.xpath("./td[3]/text()").extract()[0]
+            item['workLocation'] = node.xpath("./td[4]/text()").extract()[0]
+            item['publishTime'] = node.xpath("./td[5]/text()").extract()[0]
+            print(item)
