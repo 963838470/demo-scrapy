2cb73f741734f8bc979c2427aeedd41c8fe101c3
r720 <963838470@qq.com>
2018-01-01 19:37:53 +0800

    douban字段

--- Douban/Douban/Douban/spiders/douban.py
+++ Douban/Douban/Douban/spiders/douban.py
@@ -11,25 +11,32 @@ class DoubanSpider(scrapy.Spider):
     start_urls = ['https://movie.douban.com/top250?start=0']
 
     def parse(self, response):
-        
+
         nodes = response.css("ol.grid_view div.item")
+        print('*' * 100)
         for node in nodes:
+            title = node.css(".title::text").extract()
+            p = node.css(".bd>p::text").extract()[0].split("<br>")
+            print("n" * 100)
+            print(node)
+            print("n" * 100)
             item = DoubanItem()
-            item["Title"] = node.css(".title::text").extract()[0]
-            item["OtherTitle"] =node.css(".other::text").extract()[0].replace("&nbsp;/&nbsp;","")
-            print('*' * 100)
-            print(item["OtherTitle"])
-            print(item["OtherTitle"].replace(" / ",""))
-            print('*' * 100)
-            # item["ImgSrc"] =
-            # item["Playable"] =
-            # item["Director"] =
+            item["Title"] = title[0]
+            item["OtherTitle"] = ((title[1].replace("\xa0/\xa0", "") + "/") if len(title) > 1 else "") + \
+                node.css(".other::text").extract()[0].replace(
+                    "\xa0/\xa0", "").replace(" ", "")
+
+            item["ImgSrc"] = node.css(".pic>a>img::attr(src)").extract()[0]
+            if len(node.css(".playable::text").extract()) > 0:
+                item["Playable"] = node.css(".playable::text").extract()[0]
+            item["Director"] = p[0]
             # item["Performer"] =
-            # item["Year"] =
+            item["Year"] = node.re("(.*)\xa0/\xa0")
             # item["Country"] =
             # item["Type"] =
             # item["Score"] =
             # item["ScoreUserNum"] =
             # item["Quote"] =
-            # item["DetailLink"] =
+            item["DetailLink"] = node.css(".pic>a::attr(href)").extract()[0]
             yield item
+        print('*' * 100)
