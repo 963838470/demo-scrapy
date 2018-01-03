9082d080ab9d41fd580542d714d9e2b57dba01ad
1 <1>
2018-01-03 18:43:11 +0800

    插入完成

--- Douban/save2db/douban.py
+++ Douban/save2db/douban.py
@@ -22,23 +22,31 @@ def get_info(url):
         otherTitle = node.select('div.hd > a')[0].get_text().replace(
             '\xa0', '').replace('\n', '').replace(title + "/", "")
         imgSrc = node.select('div.pic > a > img')[0].attrs['src']
-        playable = node.select('div.hd > .playable')[0].get_text()
+        if len(node.select('div.hd > .playable')) > 0:
+            playable = node.select('div.hd > .playable')[0].get_text()
+        else:
+            playable = "false"
         year = str(info[0])
         country = str(info[1])
         score = node.select('span.rating_num')[0].get_text()
-        quote = node.select('span.inq')[0].get_text()
+        if len(node.select('span.inq')) > 0:
+            quote = node.select('span.inq')[0].get_text()
+        else:
+            quote = ""
         detailLink = node.select('div.hd > a')[0].attrs['href']
         director = re.findall('导演: (.*?)\xa0', str(node), re.S)[0]
-        performer = re.findall('主演: (.*?)<br/>', str(node), re.S)[0]
+        if len(re.findall('主演: (.*?)<br/>', str(node), re.S)) > 0:
+            performer = re.findall('主演: (.*?)<br/>', str(node), re.S)[0]
+        else:
+            performer = ""
         _type = info[2]
         scoreUserNum = node.select(
             'div.star > span:nth-of-type(4)')[0].get_text().replace('人评价', '')
-        sql = "INSERT INTO Movie (Title, OtherTitle, ImgSrc, Playable, Director, Performer, Year, Country, Type, Score, ScoreUserNum, Quote, DetailLink) VALUES ('" + title + "', %s, '" + \
-            imgSrc + "', '" + playable + "', '" + director + "', '" + performer + "', '" + year + "', '" + \
-            country + "', '" + _type + "', '" + score + "', '" + \
-            scoreUserNum + "', '" + quote + "', '" + detailLink + "')"
-        print(sql)
-        MSSQL().ExecNonQuery(sql, (otherTitle))
+        sql = "INSERT INTO Movie (Title, OtherTitle, ImgSrc, Playable, Director, Performer, Year, Country, Type, Score, ScoreUserNum, Quote, DetailLink) VALUES (%s, %s,  %s,  %s,  %s,  %s,  %s,  %s,  %s,  %s,  %s,  %s,  %s)"
+        params = (title, otherTitle, imgSrc, playable, director, performer,
+                  year, country, _type, score, scoreUserNum, quote, detailLink)
+        print(params)
+        MSSQL().ExecNonQuery(sql, params)
 
         # print(node)
 
